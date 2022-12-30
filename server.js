b19c1ef0e0303b297ac81c8f8c1c1b563fd34ac6
unknown <dyahxad.s+sbalzammo@gmail.com>
2022-12-30 14:18:03 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+require('dotenv').config()
+
+const port = process.env.PORT || process.env.MY_PORT;
+
+app.listen(port, () => {
+	console.log("Listen on :", port);
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.json({ type: "application/vnd.api+json" }));
+app.use(cors());
+
+const landing_page = require("./routes/landing_page");
+app.use(landing_page);
